8d3c6eef0145a13ad2e2668ebcb392d887827c89
Itsme-Jan <itsjanperson@gmail.com>
2025-10-30 03:00:55 -0700

    Delete real-estate directory

--- real-estate/admin/includes/admin_footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-<footer>
-    <p style="text-align:center; padding:10px; background:#2c3e50; color:#fff; margin-top:20px;">
-        &copy; <?php echo date("Y"); ?> Real Estate Admin Panel
-    </p>
-</footer>
-</body>
-</html>
